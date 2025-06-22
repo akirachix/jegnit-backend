4a6138ca98444aab928c75ba981dd7c1f1ff73c9
Mercyline Makuto <makutomercyline@gmail.com>
2025-06-22 15:14:07 +0300

    Created a payment app for agriTech project

---
+++ agriTech/payments/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+class Payment(models.Model):
+    payment_id = models.AutoField(primary_key=True)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    payment_method = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    paid_at = models.DateTimeField()
+    class Meta:
+        abstract = True
+class FarmerPayment(Payment):
+    farmer = models.ForeignKey('farmers.Farmers', on_delete=models.CASCADE)
+    cooperative = models.ForeignKey('cooperatives.Cooperatives', on_delete=models.CASCADE)
+    def __str__(self):
+        return f"FarmerPayment: {self.farmer} ({self.amount})"
+class CooperativePayment(Payment):
+    cooperative = models.ForeignKey('cooperatives.Cooperatives', on_delete=models.CASCADE)
+    supplier = models.ForeignKey('machine_supplier.Machine_Suppliers', on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.cooperative} -> {self.supplier} ({self.amount})"
