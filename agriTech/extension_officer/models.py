7a12b20f1b2e41e3ae4263f44da62cdb35dbc518
emebetgirmay <girmaayemebet@gmail.com>
2025-06-22 15:14:20 +0300

    Two apps create for the extension officer and machine supplier table

---
+++ agriTech/extension_officer/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Extension_Officers(models.Model):
+    extension_id = models.AutoField(primary_key=True)
+    cooperative_id = models.ForeignKey(Cooperatives, on_delete=models.CASCADE)
+    officer_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    created_at = models.DateTimeField()
+    def __str__(self):
+        return self.officer_name
