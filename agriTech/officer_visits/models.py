b50142838aa4270e0c535208cfb9957e22d779e3
paulinemwihakimuiruri <paulinemwihakimuiruri@gmail.com>
2025-06-22 15:49:31 +0300

    Created the lending records table and officer visits table

--- agriTech/officer_visits/models.py
+++ agriTech/officer_visits/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from farmers.models import Farmer
+from extension_officer.models import Extension_Officer
 
 # Create your models here.
 class Officer_Visit(models.Model):
     visits_id = models.AutoField(primary_key=True)
-    # officer_id = models.ForeignKey(Extension_Officers, on_delete=models.CASCADE)
+    officer_id = models.ForeignKey(Extension_Officer, on_delete=models.CASCADE)
     farmer_id = models.ForeignKey(Farmer, on_delete=models.CASCADE)
     visit_date = models.DateTimeField()
     notes = models.TextField()
