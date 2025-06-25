09154a638343e54c43f615018c7b3557cd8b2a2c
paulinemwihakimuiruri <paulinemwihakimuiruri@gmail.com>
2025-06-25 13:49:35 +0300

    Created serializer file, urls file and performed tests for each app

--- agriTech/lending_records/views.py
+++ agriTech/lending_records/views.py
@@ -1,3 +1,11 @@
+
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from lending_records.models import Lending_Record
+from .serializers import Lending_RecordSerializer
+
+class Lending_RecordViewSet(viewsets.ModelViewSet):
+    queryset = Lending_Record.objects.all()
+    serializer_class = Lending_RecordSerializer
