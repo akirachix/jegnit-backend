83092142559d4a8c2fb44c918501cd61ccbd46bf
emebetgirmay <girmaayemebet@gmail.com>
2025-07-19 10:37:48 +0300

    Merged apps

--- agriTech/api/serializers.py
+++ agriTech/api/serializers.py
@@ -7,36 +7,28 @@ from payments.models import Lending_Record
 from users.models import User
 
 
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Payment
-#         fields = '__all__'
-    # def validate_amount(self, value):
-    #     if value <= 0:
-    #         raise serializers.ValidationError("Amount must be positive.")
-        # return value
-    # def validate(self, attrs):
-    #     user = attrs.get('user')
-    #     payment_type = attrs.get('payment_type')
-    #     farmer = attrs.get('farmer')
-    #     cooperative = attrs.get('cooperative')
-    #     supplier = attrs.get('supplier')
-    #     if not user:
-    #         raise serializers.ValidationError("Payment must have a user.")
-    #     if payment_type == 'farmer_to_coop':
-    #         if not farmer or not cooperative:
-    #             raise serializers.ValidationError("Farmer and cooperative must be set for farmer_to_coop payment.")
-    #     elif payment_type == 'coop_to_supplier':
-    #         if not cooperative or not supplier:
-    #             raise serializers.ValidationError("Cooperative and supplier must be set for coop_to_supplier payment.")
-    #     return attrs
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields='__all__'
-
-
+        fields = ['user_id',
+        'type',
+        'email',
+        'password',
+        'phone_number',
+        'created_at',
+        'last_login',
+        'date_joined',
+        'cooperative_name',
+        'officer_name',
+        'farmer_name',
+        'supplier_name',
+        'cooperative',
+        ]
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
     def validate(self, data):
         user_type = data.get('type')
         errors = {}
@@ -57,6 +49,11 @@ class UserSerializer(serializers.ModelSerializer):
         if errors:
             raise serializers.ValidationError(errors)
         return data
+    phone_number = serializers.RegexField(
+        regex = r'^\+?[0-9]{10,15}$',
+        error_messages = {'invalid':
+        'Phone number must contain only digits and optional + at the beginning.'}
+    )
 
 class MachineryTrackingSerializer(serializers.ModelSerializer):
     class Meta:
@@ -71,11 +68,26 @@ class MachinerySerializer(serializers.ModelSerializer):
         model = Machinery
         fields = '__all__'
 class Lending_RecordSerializer(serializers.ModelSerializer):
+    borrower = serializers.PrimaryKeyRelatedField(
+        queryset = User.objects.filter(type__in =[
+            'farmer', 'cooperative'
+        ])
+    )
+    approved_by =serializers.PrimaryKeyRelatedField(
+        queryset = User.objects.filter(type__in=[
+            'cooperative', 'machine_supplier'
+        ])
+    )
+    
     class Meta:
         model = Lending_Record
         fields = '__all__'
-
 class PaymentSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        queryset = User.objects.filter(type__in =[
+            'farmer', 'cooperative'
+        ])
+    )
     class Meta:
         model = Payment
         fields = '__all__'
--- agriTech/payments/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import FarmerPaymentViewSet, CooperativePaymentViewSet
-# router = DefaultRouter()
-# router.register(r'farmer_payments', FarmerPaymentViewSet, basename='farmer_payments')
-# router.register(r'cooperative_payments', CooperativePaymentViewSet, basename='cooperative_payments')
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
