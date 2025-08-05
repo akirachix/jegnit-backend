a7b7e4bf0bc76b5e4ef2daf2cc3e90609533a89f
BerissaMuyizere <muyizereberissa@gmail.com>
2025-08-05 17:08:25 +0300

    re-created migrations and static files

--- agriTech/urls.py
+++ agriTech/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 
 from rest_framework.authtoken.views import obtain_auth_token
@@ -16,3 +17,6 @@ urlpatterns = [
 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
