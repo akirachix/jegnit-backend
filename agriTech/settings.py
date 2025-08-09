7b798b73bdf905901a2f20befaf52d78aa619da0
Joyce Ndichu <ndichujoyce8@gmail.com>
2025-08-09 21:34:23 +0300

    Update settings.py

--- agriTech/settings.py
+++ agriTech/settings.py
@@ -65,11 +65,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'agriTech.wsgi.application'
 
-DATABASE_URL = os.getenv('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}')
-
-DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=False)
-}
+DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
+if not os.getenv("DATABASE_URL"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
